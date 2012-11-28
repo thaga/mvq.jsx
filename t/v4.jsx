44eadd944df4d2f86f2ad698cfd515a1fee0cf48
Fuji, Goro <g.psy.va@gmail.com>
2012-11-28 11:22:22 +0900

    add skelton tests

---
+++ t/v4.jsx
@@ -0,0 +1,20 @@
+// tests are borrowed from Google Closure Library
+// https://developers.google.com/closure/library/
+
+import "js/web.jsx";
+
+import "../lib/mvq.jsx";
+
+import "./lib/test-mvq.jsx";
+
+class _Test extends TestMVQ.<V4> {
+
+    function testConstructor() : void {
+        var v0 = new V4();
+        this.assertEquals(
+            [0, 0, 0, 0], v0);
+    }
+
+}
+
+// vim: set expandtab:
